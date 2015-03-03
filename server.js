591cddb448e84056e54b61d32f1f0c2b1a8fe6a0
MikeMugu <mikemugu@gmail.com>
2015-03-02 20:50:08 -0500

    Update for CORS.

--- server.js
+++ server.js
@@ -190,7 +190,7 @@ var CapitalAreaMSC = function() {
         self.contentBlocks = require('contentblocks')(
         { 
             app: self.app, 
-            host: 'http://www.capitalareamsc.com', 
+            host: self.host, 
             pathFind: '/content/find?q={"@subject":"[id]"}',
             pathPost: '/content', 
             pathPut: '/content/[id]', 
